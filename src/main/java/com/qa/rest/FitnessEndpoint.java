f18db2993d26ba61125ae4aa02d2af39ae4378c5
Taoheed1 <taoheed123afolayan@gmail.com>
2019-01-25 09:33:08 +0000

    Endpoint adjust for new program service classes

--- src/main/java/com/qa/rest/FitnessEndpoint.java
+++ src/main/java/com/qa/rest/FitnessEndpoint.java
@@ -11,58 +11,66 @@ import javax.ws.rs.Produces;
 
 import org.apache.log4j.Logger;
 
-import com.qa.business.service.FitnessService;
+import com.qa.business.service.ProgramService;
+import com.qa.business.service.UserService;
 
 @Path("/classroom")
 public class FitnessEndpoint {
 
 	@Inject
-	private FitnessService service;
+	private UserService service_user;
+	
+	@Inject
+	private ProgramService service_prog;
 
 	@Path("/getAllUsers")
 	@GET
 	@Produces({ "application/json" })
 	public String getAccounts() {
-		return service.getAllUsers();
+		return service_user.getAllUsers();
 	}
 
 	@Path("/getAllPrograms")
 	@GET
 	@Produces({ "application/json" })
 	public String getPrograms() {
-		return service.getAllPrograms();
+		return service_prog.getAllPrograms();
 	}
 
 	@Path("/createAccount")
 	@POST
 	@Produces({ "application/json" })
 	public String addAccount(String account) {
-		return service.addNewAccount(account);
+		return service_user.addNewAccount(account);
 	}
 
 	@Path("/deleteAccount/{id}")
 	@DELETE
 	@Produces({ "application/json" })
 	public String deleteAccount(@PathParam("id") Long userID) {
-		return service.deleteUser(userID);
+		return service_user.deleteUser(userID);
 	}
 
 	@Path("/updateUser/{id}")
 	@PUT
 	@Produces({ "application/json" })
 	public String updateAccount(@PathParam("id") Long userID, String account) {
-		return service.updateUser(userID, account);
+		return service_user.updateUser(userID, account);
 	}
 
 	@Path("/updateProgram/{id}")
 	@PUT
 	@Produces({ "application/json" })
 	public String updatePrograms(@PathParam("id") Long programID, String program) {
-		return service.updateProgram(programID, program);
+		return service_prog.updateProgram(programID, program);
 	}
 
-	public void setService(FitnessService service) {
-		this.service = service;
+	public void setService(UserService service_user) {
+		this.service_user = service_user;
+	}
+	
+	public void setService(ProgramService service_prog) {
+		this.service_prog = service_prog;
 	}
 
 }
