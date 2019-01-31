f383c2c8d3bcf08047bf7444af973619da2e565b
Taoheed1 <taoheed123afolayan@gmail.com>
2019-01-31 18:29:57 +0000

    IDs removed from import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO PROGRAM (programID, programName, exerciseName, reps, sets, programType ) VALUES (10, 'Insanity', 'Mountain climbers', 30, 3, Cardio)
-INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES (1, 'TheGreatWhitJew', 'Qwerty123', 'beginner',2)
+INSERT INTO PROGRAM (programName, exerciseName, reps, sets, programType ) VALUES ('Insanity', 'Mountain climbers', 30, 3, Cardio)
+INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES ('TheGreatWhitJew', 'Qwerty123', 'beginner',2)
 
-INSERT INTO PROGRAM (programID, programName, exerciseName, reps, sets, programType) VALUES (20, 'Insanity', 'Burpees', 10, 3, Cardio)
-INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES (2, 'TaomanianDevil', 'password', 'intermediate',3)
+INSERT INTO PROGRAM (programName, exerciseName, reps, sets, programType) VALUES ('Insanity', 'Burpees', 10, 3, Cardio)
+INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES ('TaomanianDevil', 'password', 'intermediate',3)
 
-INSERT INTO PROGRAM (programID, programName, exerciseName, reps, sets, programType) VALUES (30, 'Yoga 1', 'Downward facing dog', 30, 30, Other)
-INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES (3, 'TheLeet, 'doYouEvenLift', 'expert',4)
-INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES (2, 'TaomanianDevil', 'password', 'intermediate',3)
+INSERT INTO PROGRAM (programName, exerciseName, reps, sets, programType) VALUES ('Yoga 1', 'Downward facing dog', 30, 30, Other)
+INSERT INTO ACCOUNT (userName, password, proficiency, programID ) VALUES ('TheLeet, 'doYouEvenLift', 'expert',4)
+INSERT INTO ACCOUNT (userName, password, proficiency, programID ) VALUES ('TaomanianDevil', 'password', 'intermediate',3)
 
 
-INSERT INTO PROGRAM (programID, programName, exerciseName, reps, sets, programType) VALUES (40, 'Home Ex', 'Pyramid pushups', 16, 2, HomeWorkouts)
-INSERT INTO ACCOUNT (UserID, userName, password, proficiency, programID ) VALUES (2, 'Greg81', 'gregory', 'beginner',3)
+INSERT INTO PROGRAM (programName, exerciseName, reps, sets, programType) VALUES ('Home Ex', 'Pyramid pushups', 16, 2, HomeWorkouts)
+INSERT INTO ACCOUNT (userName, password, proficiency, programID ) VALUES ('Greg81z', 'gregory', 'beginner',3)
